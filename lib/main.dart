994cedb7578946f01f5f875a50d6ef9f0d31d8bf
Amine-Smahi <mohammed.amine.smahi@gmail.com>
2018-05-28 00:39:09 +0100

    Add landing_page widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import './pages/landing_page.dart';
 void main(){
-   runApp(new MaterialApp());
+   runApp(new MaterialApp(
+     home: new LandingPage(),
+   ));
 }
