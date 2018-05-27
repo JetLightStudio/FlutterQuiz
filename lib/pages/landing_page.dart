a5816306ef8d95f6faa46ea8dcaeaa5fd7096188
Amine-Smahi <mohammed.amine.smahi@gmail.com>
2018-05-28 00:49:23 +0100

    Add better ui to the landing_page

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -5,6 +5,16 @@ class LandingPage extends StatelessWidget{
   Widget build(BuildContext context){
     return new Material(
       color: Colors.greenAccent,
+      child: new InkWell(
+        onTap: ()=> print("zzz"),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text("Lets Quizzz",style: TextStyle(color: Colors.white,fontSize: 50.0,fontWeight:  FontWeight.bold)),
+            new Text("Tap to Start!",style: TextStyle(color: Colors.white,fontSize: 20.0,fontWeight:  FontWeight.bold))
+          ],
+        ),
+      ),
     );
   }
 }
