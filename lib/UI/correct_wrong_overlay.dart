da0ed54fa171bb83a4662dd5a032950636bb871e
Amine-Smahi <mohammed.amine.smahi@gmail.com>
2018-06-05 23:33:37 +0100

    Add Animation

--- lib/UI/correct_wrong_overlay.dart
+++ lib/UI/correct_wrong_overlay.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 
@@ -9,7 +11,19 @@ class CorrectWrongOverlay extends StatefulWidget{
  @override
  State createState()=> new CorrectWrongOverlayState();
 }
-class CorrectWrongOverlayState extends State<CorrectWrongOverlay>{
+class CorrectWrongOverlayState extends State<CorrectWrongOverlay> with SingleTickerProviderStateMixin{
+  Animation<double> _iconAnimation;
+  AnimationController _iconAnimationController;
+  
+  @override
+  void initState(){
+    super.initState();
+    _iconAnimationController = new AnimationController(duration: new Duration(seconds: 2),vsync: this);
+    _iconAnimation = new CurvedAnimation(parent: _iconAnimationController,curve: Curves.elasticOut);
+    _iconAnimation.addListener(()=> this.setState((){}));
+    _iconAnimationController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
       return new Material(
@@ -24,7 +38,13 @@ class CorrectWrongOverlayState extends State<CorrectWrongOverlay>{
                   color: Colors.white,
                   shape: BoxShape.circle
                 ),
-                child: new Icon(widget._isCorrect == true ? Icons.done : Icons.clear,size: 80.0),
+                child: new Transform.rotate(
+                  angle: _iconAnimation.value * 2 * PI,
+                  child: new Icon(widget._isCorrect == true ? Icons.done : Icons.clear,size: _iconAnimation.value * 80.0),
+                )
+              ),
+              new Padding(
+                padding: new EdgeInsets.only(bottom: 10.0),
               ),
               new Text(widget._isCorrect == true ? "Correct!" : "Wrong!" ,style: new TextStyle(fontSize: 30.0,color: Colors.white),)
             ],
